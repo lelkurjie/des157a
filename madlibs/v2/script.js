ad246f8d5a4cfbbdce06ef3df639180cce3a880c
Leila Elkurjie <leilaelkurjie@Leilas-MacBook-Air-7.local>
2025-02-11 21:59:39 -0800

    v2

--- madlibs/v2/script.js
+++ madlibs/v2/script.js
@@ -6,11 +6,16 @@
     const madlibOverlay = document.querySelector('#madlib-overlay');
     const madlib = document.querySelector('#madlib');
     const ad1input = document.querySelector('#ad1');
+
+    console.log(ad1input.value);
+
     const cityinput = document.querySelector('#city');
     const verbinput = document.querySelector('#verb');
     const ad2input = document.querySelector('#ad2');
     const numberinput = document.querySelector('#num');
     const nouninput = document.querySelector('#noun');
+
+    console.log(document.querySelector('#ad1').value);
 
     // document.querySelector("#submit").addEventListener("click", function() {
     //     document.querySelector("#madlibOverlay").style.display = "block";
@@ -19,7 +24,12 @@
     form.addEventListener('submit', function(event){
         event.preventDefault();
 
+        console.log('submitted form');
+        console.log(ad1input);
+
         const ad1 = document.querySelector('#ad1').value;
+        console.log(document.querySelector('#ad1').value);
+
         const city = document.querySelector('#city').value;
         const verb = document.querySelector('#verb').value;
         const ad2= document.querySelector('#ad2').value;
@@ -61,9 +71,9 @@
             // document.querySelector("#cafeimg").style.display = "none";
 
 
-            const myText = `It was a <span>${ad1}</span> morning when college student Rose walked into a cafe, located in a quaint corner of  <span>${city}</span>.  The cafe was beautiful and had a calm atmosphere, and the scent of coffee filled the air. The barista, Claire, was busy <span>${verb}</span> behind the counter. “Good morning!” she says, with a bright smile. “What can I get for you?”. Rose replies, “hmm, I will take a <span>${ad2}</span> latte please!”. “Of course. That will be <span>${number}</span> please.” Rose pays and her drink is made. She sits by the window and sees a cute <span>${noun}</span> outside, playing fetch. Rose giggles. It’s a beautiful morning, one that she will always remember.`;
+            madlib.innerHTML = `It was a <span>${ad1}</span> morning when college student Rose walked into a cafe, located in a quaint corner of  <span>${city}</span>.  The cafe was beautiful and had a calm atmosphere, and the scent of coffee filled the air. The barista, Claire, was busy <span>${verb}</span> behind the counter. “Good morning!” she says, with a bright smile. “What can I get for you?”. Rose replies, “hmm, I will take a <span>${ad2}</span> latte please!”. “Of course. That will be <span>${number}</span> please.” Rose pays and her drink is made. She sits by the window and sees a cute <span>${noun}</span> outside, playing fetch. Rose giggles. It’s a beautiful morning, one that she will always remember.`;
 
-            madlib.innerHTML = myText;
+            //  = myText;
 
             document.querySelector('#ad1').value = '';
             document.querySelector('#city').value = '';
